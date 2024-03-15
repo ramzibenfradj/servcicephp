f9dc904c95f39807bc43ce65ffd87b3294a011ab
Ramzi_Benfraj <74455139+ramzibenfradj@users.noreply.github.com>
2024-03-15 16:47:18 +0000

    Update config.php

--- var/www/html/config.php
+++ var/www/html/config.php
@@ -1,7 +1,7 @@
 <?php
-$user="root";
-$mp="";
-$database="locationdatabase";
-$server="127.0.0.1";
+$user="sql8691403";
+$mp="5V3uuiKrkH";
+$database="sql8691403";
+$server="sql8.freesqldatabase.com";
 $port="3306";
 ?>
